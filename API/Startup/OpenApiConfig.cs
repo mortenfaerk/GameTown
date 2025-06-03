f73ea73daa9179cba762ca3f6973579ca74dcbbc
Morten Færk Rygaard <mortenchr@gmail.com>
2025-06-04 00:06:47 +0200

    Added OpenAPI Config for Scalar to allow JWT testing in GUI.

--- API/Startup/OpenApiConfig.cs
+++ API/Startup/OpenApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using Scalar.AspNetCore;
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
 
 namespace API.Startup;
 
@@ -6,7 +8,27 @@ public static class OpenApiConfig
 {
     public static void AddOpenApiServices(this IServiceCollection services)
     {
-        services.AddOpenApi();
+        services.AddOpenApi("v1", options => {
+            options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
+            options.AddDocumentTransformer((document, context, _) =>
+            {
+                document.Info = new()
+                {
+                    Title = "GameTown",
+                    Version = "v1",
+                    Description = """
+
+                """,
+                    Contact = new()
+                    {
+                        Name = "",
+                        Email = "",
+                        Url = new Uri("https://gametown.local/api")
+                    }
+                };
+                return Task.CompletedTask;
+            });
+        });
     }
 
     public static void UseOpenApi(this WebApplication app)
@@ -21,6 +43,37 @@ public static class OpenApiConfig
                 options.Layout = ScalarLayout.Modern;
                 options.HideClientButton = true;
             });
+        }
+    }
+}
+
+internal sealed class BearerSecuritySchemeTransformer(Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+        if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
+        {
+            var requirements = new Dictionary<string, OpenApiSecurityScheme>
+            {
+                ["Bearer"] = new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    In = ParameterLocation.Header,
+                    BearerFormat = "Json Web Token"
+                }
+            };
+            document.Components ??= new OpenApiComponents();
+            document.Components.SecuritySchemes = requirements;
+
+            foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations))
+            {
+                operation.Value.Security.Add(new OpenApiSecurityRequirement
+                {
+                    [new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } }] = Array.Empty<string>()
+                });
+            }
         }
     }
 }
